5f8af332bbf248b6f128364e28bcfbfe836994af
ikromyusuf <ikromyusupov09@gmail.com>
2023-03-10 23:35:18 +0500

    changed instruction

--- begin10.py
+++ begin10.py
@@ -6,6 +6,6 @@
 
 # Assign 8 (int value) to 'd' variable;
 
-# Find the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin10' to variable 'k';
+# Find the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin10' to variable 'k';
 
 # # Print the variable 'k'.
