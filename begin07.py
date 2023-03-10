1b010cb08acf59f6e4278118d0bb3742704699fd
ikromyusuf <ikromyusupov09@gmail.com>
2023-03-10 23:21:55 +0500

    link changed

--- begin07.py
+++ begin07.py
@@ -4,4 +4,4 @@
 
 # Assign 8 (int value) to 'c' variable;
 
-# Print the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin07';
+# Print the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin07'
